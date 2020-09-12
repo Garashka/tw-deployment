3342f2b31f1a531105b105ca3a4728220888ae05
Garashka <jgoodsir1210@gmail.com>
2020-09-12 14:47:30 +1000

    init

---
+++ precache-manifest.837d2554d8940c2160afbf386abc7801.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19024ffdaa0c1a75951fac77e1be4830",
+    "url": "/tw-deployment/index.html"
+  },
+  {
+    "revision": "64fdae28ef14808a9976",
+    "url": "/tw-deployment/static/css/main.5ad7abeb.chunk.css"
+  },
+  {
+    "revision": "301b7a0d62a0abbc4198",
+    "url": "/tw-deployment/static/js/2.6200db47.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/tw-deployment/static/js/2.6200db47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64fdae28ef14808a9976",
+    "url": "/tw-deployment/static/js/main.b6769a8f.chunk.js"
+  },
+  {
+    "revision": "d2ccf3170378ebb55d52",
+    "url": "/tw-deployment/static/js/runtime-main.952a250d.js"
+  },
+  {
+    "revision": "8dad80289a1ece380591ad143b616747",
+    "url": "/tw-deployment/static/media/logo.8dad8028.svg"
+  }
+]);
